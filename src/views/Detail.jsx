b8169e8edec60b742dbe1e46be52b49d2b36bd41
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-06-14 16:58:44 -0300

    He agregado todo lo necesario para comenzar a trabajar

--- src/views/Detail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from '../assets/styles/components/views/Detail.module.css';
-
-const Detail = () => {
-	return <div>Este es el detail</div>;
-};
-
-export default Detail;
