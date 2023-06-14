9da48e0ade0655dfc8e350d991a3d52f0edb26a5
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-06-14 16:04:48 -0300

    feature/dev-Miguel (frontend/src): I have made the changes in App and Index

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from "../assets/styles/components/Nav.module.css";
+
+const Nav = () => {
+  return (
+    <div>
+      Este es el Nav
+    </div>
+  )
+}
+
+export default Nav
