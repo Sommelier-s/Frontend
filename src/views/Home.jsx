267fef53c70dc68847152e6cd094c8d9ae4b404e
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-06-15 19:32:18 -0500

    feature/dev-Pedro (frontend/home): Prueba de home con estilos

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 import styles from '../assets/styles/components/views/Home.module.css';
 const Home = () => {
   return (
-    <div>
-      <h1 className={styles.title}>Hola este es el inicio</h1>
-      <h1 className={styles.title}>Bienvenidos a Sommelier!</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>LICOR DEL MES</h1>
+      <div className={styles.featuredCard}>
+        <img src="https://img.freepik.com/iconos-gratis/botella-vino_318-61391.jpg" alt="Licor del Mes" />
+        <div className={styles.featuredCardContent}>
+          <h3 className={styles.featuredCardTitle}>Licor de junio</h3>
+          <p className={styles.featuredCardDescription}>Descripción del licor del mes</p>
+        </div>
+      </div>
+
+      <h1 className={styles.title}>LOS MÁS VENDIDOS</h1>
+      <div className={styles.carrusel}>
+        <div className={styles.carruselItem}>
+          <img src="https://img.freepik.com/iconos-gratis/botella-vino_318-61391.jpg" alt="Vino 1" />
+          <h3>Vino 1</h3>
+          <p>Este es un vino viejo</p>
+        </div>
+        <div className={styles.carruselItem}>
+          <img src="https://img.freepik.com/iconos-gratis/botella-vino_318-61391.jpg" alt="Vino 2" />
+          <h3>Vino 2</h3>
+          <p>Este es un vino viejo</p>
+        </div>
+        <div className={styles.carruselItem}>
+          <img src="https://img.freepik.com/iconos-gratis/botella-vino_318-61391.jpg" alt="Vino 3" />
+          <h3>Vino 3</h3>
+          <p>Este es un vino viejo</p>
+        </div>
+        <div className={styles.carruselItem}>
+          <img src="https://img.freepik.com/iconos-gratis/botella-vino_318-61391.jpg" alt="Vino 3" />
+          <h3>Vino 4</h3>
+          <p>Este es un vino viejo</p>
+        </div>
+        <div className={styles.carruselItem}>
+          <img src="https://img.freepik.com/iconos-gratis/botella-vino_318-61391.jpg" alt="Vino 3" />
+          <h3>Vino 5</h3>
+          <p>Este es un vino viejo</p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
