7da1e33d9ecc3e07acd9514d689429212ef9139a
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-06-27 21:17:58 -0500

    Feature/dev-PedroR (Frontend/Dashboar Admin): Arreglos de diseño. Se agrego la searchBar

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -11,8 +11,48 @@ import CreateCategory from '../components/CreateCategory';
 import imageBack from "../assets/img/imageBack.png";
 
 export default function Dashboard() {
+	const users = [
+		{
+			"id":"d7f80a8b-8d53-498a-95ab-46ba10775264",
+			"first_name":"Andrea",
+			"last_name":"Rincon",
+			"date_birth":"1983-11-27",
+			"email":"pedrofaro2711@gmail.com",
+			"profile_picture":"https://ionicframework.com/docs/img/demos/avatar.svg",
+			"isAdmin":false
+		},
+		{
+			"id":"d7f80a8b-8d53-498a-95ab-46ba10775264",
+			"first_name":"Miguel",
+			"last_name":"Fernandez",
+			"date_birth":"1983-11-27",
+			"email":"pedrofaro2711@gmail.com",
+			"profile_picture":"https://ionicframework.com/docs/img/demos/avatar.svg",
+			"isAdmin":false
+		},
+		{
+			"id":"d7f80a8b-8d53-498a-95ab-46ba10775264",
+			"first_name":"Gonzalo",
+			"last_name":"Suarez",
+			"date_birth":"1983-11-27",
+			"email":"pedrofaro2711@gmail.com",
+			"profile_picture":"https://ionicframework.com/docs/img/demos/avatar.svg",
+			"isAdmin":false
+		},
+		{
+			"id":"d7f80a8b-8d53-498a-95ab-46ba10775264",
+			"first_name":"Pedro",
+			"last_name":"Romero",
+			"date_birth":"1983-11-27",
+			"email":"pedrofaro2711@gmail.com",
+			"profile_picture":"https://ionicframework.com/docs/img/demos/avatar.svg",
+			"isAdmin":false
+		},
+	
+	]
 	const dispatch = useDispatch();
 	const [selectedOption, setSelectedOption] = useState('profile'); // Estado para almacenar la opción seleccionada del menú
+	const [showSearchBar, setShowSearchBar] = useState(false);
 
 	const allWine = useSelector((state) => state.wine);
 	const allLiquor = useSelector((state) => state.liquor);
@@ -25,7 +65,14 @@ export default function Dashboard() {
 		}
 		dispatch(getAllWine());
 		dispatch(getAllLiquor());
-	}, []);
+
+		if (selectedOption === 'wine' || selectedOption === 'liquor') {
+			setShowSearchBar(true);
+		} else {
+			setShowSearchBar(false);
+		}
+
+	}, [selectedOption]);
 
 	let filteredData;
 	switch (selectedOption) {
@@ -42,7 +89,7 @@ export default function Dashboard() {
 			filteredData = allLiquor;
 			break;
 		case 'users':
-			filteredData = [user];
+			filteredData = users;
 			break;
 		default:
 			filteredData = [];
@@ -69,36 +116,53 @@ export default function Dashboard() {
 				<DashboardMenu onClick={handleMenu} />
 				
 			</div>
-			{selectedOption === 'profile' && (
-				<DashboardCardAdmin
-					profile_picture = {user.profile_picture}
-					name={`${user.first_name} ${user.last_name}`}
-					email = {user.email}
-					date_birth = {user.date_birth}
-					password = "**********"
-				/>
-			)}
-			{selectedOption === 'users' && ( // Renderiza las cards solo si se ha seleccionado una  opción
-				<DashboardCardAdminUsers
-					profile_picture = {user.profile_picture}
-					name={`${user.first_name} ${user.last_name}`}
-					email = {user.email}
-					date_birth = {user.date_birth}
-				/>
-			)}	
-			{selectedOption !== "users" && filteredData.length > 0 && (
-				<div className={styles['dashboard-card-container']}>
-					{filteredData.map((item) => (
-						<DashboardCard
-							key={item.id}
-							name={item.name} // Propiedad "name" desde el estado
-							description={item.description} // Propiedad "description" desde el estado
-							stock={item.stock} // Propiedad "stock" desde el estado
-							picture={item.picture} // Propiedad "picture" desde el estado
-						/>
-					))}
-				</div>
-			)}
+			<div className={styles.cardProfile}>
+				{selectedOption === 'profile' && (
+					<DashboardCardAdmin
+						profile_picture = {user.profile_picture}
+						name={`${user.first_name} ${user.last_name}`}
+						email = {user.email}
+						date_birth = {user.date_birth}
+						password = "**********"
+					/>
+				)}
+			</div>
+			<div className={styles.cardProfileUser}>
+				{selectedOption === 'users' && (
+					<div>
+						{users.map((user) => (
+							<DashboardCardAdminUsers
+								key={user.id}
+								profile_picture = {user.profile_picture}
+								name={`${user.first_name} ${user.last_name}`}
+								email = {user.email}
+								date_birth = {user.date_birth}
+							/>
+						))}
+					</div>
+				)}
+			</div>
+			<div className={styles.cardsContainer}>
+				{showSearchBar && (
+					<div className={styles.searchBar}>
+						<input type="text" placeholder="Buscar..." />
+						<button type="submit">Buscar</button>
+					</div>
+				)}
+				{selectedOption !== "users" && filteredData.length > 0 && (
+					<div className={styles['dashboard-card-container']}>
+						{filteredData.map((item) => (
+							<DashboardCard
+								key={item.id}
+								name={item.name} // Propiedad "name" desde el estado
+								description={item.description} // Propiedad "description" desde el estado
+								stock={item.stock} // Propiedad "stock" desde el estado
+								picture={item.picture} // Propiedad "picture" desde el estado
+							/>
+						))}
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
