5b90cd0ab5e2ee5295f19cdcd81f532deb3f9b29
JesseMcCallums <jessemccallums2001@gmail.com>
2023-06-28 00:07:10 -0400

    Feature Purchased Products
    In this commit I am adding the products purchased by the user on the dashboard

--- src/views/DashboardUser.jsx
+++ src/views/DashboardUser.jsx
@@ -1,22 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import DashboardMenuUser from "../components/DashboardMenuUser";
 import DashboardCardUser from "../components/DashboardCardUser";
 import DashboardCardProductUser from '../components/DashboardCardProductUser';
+import axios from 'axios';
+
 
 import styles from "../assets/styles/components/views/DashboardUser.module.css";
 
 const DashboardUser = () => {
 	const cartProducs = useSelector((state) => state.cart);
 	const [selectedOption, setSelectedOption] = useState('');
+	const [purchasedProducts, setPurchasedProducts] = useState([]);
+	const [productsSaved, setProductsSaved] = useState([]);
 	const navigate = useNavigate();
-
-	const handleMenu = (option) => {
-		setSelectedOption(option);
+	const userID = 'e2450f22-c955-4399-8d51-944db3703f07';
+	
+	useEffect(() => {
+		getPurchasedProducts();
+	}, []);
+	
+	const getPurchasedProducts = async () => {
+		try {
+			const { data } = await axios.get(`http://localhost:3001/purchased/${userID}`);
+			setPurchasedProducts(data.data);
+			await fetchProductDetails();
+		} catch (error) {
+			console.log(error);
+		}
 	};
-
-	return (
+	
+	const fetchProductDetails = async () => {
+		try {
+			const productDetails = await Promise.all(
+				purchasedProducts.map(async (el) => {
+					const { data } = await axios.get(`http://localhost:3001/wine/${el.product_id}`);
+					return data.data;
+				})
+				);
+				setProductsSaved(productDetails);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		
+		const handleMenu = (option) => {
+			setSelectedOption(option);
+		};
+		
+		console.log(productsSaved);
+		return (
 		<div className={styles.container}>
 			<div className={styles.menuContainer}> 
 				<DashboardMenuUser onClick={handleMenu} />
@@ -34,12 +68,13 @@ const DashboardUser = () => {
 						/>
 						)}
 					{selectedOption === 'purchasedProducts' && (
-						cartProducs.map((product) => (
+						productsSaved.map((product) => (
 							<DashboardCardProductUser
 							key={product.id}
 							product={product}
 							/>
 						))
+						
 					)}
 				</div>
 			)}
