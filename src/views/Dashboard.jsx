19b34e3b8e7da218fa46b475a5094f6dbdf74b2a
gonzalo <gonzasuarez100@gmail.com>
2023-06-30 11:35:54 -0300

    features/dev-gonzaS(Frontend/dashboard):Agregar nueva categoria y modificacion en la seccion de usuarios

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -10,6 +10,7 @@ import styles from '../assets/styles/components/views/Dashboard.module.css';
 import { useNavigate } from 'react-router-dom';
 import CreateCategory from '../components/CreateCategory';
 import imageBack from '../assets/img/imageBack.png';
+import AddCategoryForm from '../components/AddCategoryForm';
 
 export default function Dashboard() {
 	const users = [
@@ -223,6 +224,14 @@ export default function Dashboard() {
 						</div>
 					)}
 				</div>
+
+			<div className={styles.cardsContainer}>
+				{selectedOption === 'load new category' && (
+					< AddCategoryForm />
+				)}
+			</div>
+			
+
 			</div>
 		</div>
 	);
