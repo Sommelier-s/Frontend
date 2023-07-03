0de5a692cac95fcb6936e08990d671deaaf3ccdf
JesseMcCallums <jessemccallums2001@gmail.com>
2023-07-03 17:19:24 -0400

    Feature(reviewsbyuser)
    In this commit I am adding a functionality to get all the review made by user

--- src/components/DashboardCardProductUserReview.jsx
+++ src/components/DashboardCardProductUserReview.jsx
@@ -1,53 +1,102 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardContent, Typography, Button } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { Rating } from '@mui/material';
 
 import styles from "../assets/styles/components/DashboardUserReview.module.css";
+import jack_copy from "../assets/img/jack_copy.png";
 
-import jack_copy from "../assets/img/jack_copy.png"
+const DashboardCardProductUserReview = () => {
+  const user = useSelector((state) => state.user);
+  const idUser = user.id;
+  const [ratings, setRatings] = useState([]);
+  const [getAll, setGetAll] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
 
+  const getRatings = async () => {
+    try {
+      const { data } = await axios.get(`/rating/user/${idUser}`);
+      setRatings(data.data);
+    } catch (error) {
+      console.log('Error fetching ratings:', error);
+    }
+  };
 
+  const getProducts = async () => {
+    try {
+      const promises = ratings.map((info) => {
+        return axios.get(`/both_drinks/?id=${info.wine_id ? info.wine_id : info.liquor_id}`);
+      });
 
-const DashboardCardProductUserReview = ({
+      const responses = await Promise.all(promises);
+      const data = responses.map((response) => response.data.data);
+      setGetAll(data);
+    } catch (error) {
+      console.log('Error fetching products:', error);
+    }
+  };
 
-}) => {
+  useEffect(() => {
+    getRatings();
+    getProducts();
+  }, []);
 
-    const [isEditing, setIsEditing] = useState(false);
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
 
-    const handleEditClick = () => {
-        setIsEditing(true);
-    };
+  console.log(ratings);
+  console.log(getAll);
 
-    return (
-        <Card className={styles.card}>
-            <div className={styles.imageItems}>
-                <img src={jack_copy} alt="img" className={styles.productImage} />
+  return (
+    <>
+      {ratings.length !== 0 ? (
+        ratings.map((rating, index) => {
+          const product = getAll[index];
+          return (
+            <Card key={rating.id} className={styles.card}>
+              <div className={styles.imageItems}>
+                <img src={product?.picture || jack_copy} alt="img" className={styles.productImage} />
 
                 <div className={styles.productInfo}>
-                    <Typography variant="h4" color="text.secondary">
-                        Nombre del producto
-                    </Typography>
+                  <Typography variant="h4" color="text.secondary">
+                    {product?.name || 'Nombre del producto'}
+                  </Typography>
 
-                    <Typography variant="body1" color="text.secondary" className={styles.comment}>
-                        Comentario: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-                    </Typography>
+                  <Typography variant="body1" color="text.secondary" className={styles.comment}>
+                    Comentario: "{rating.comment}"
+                  </Typography>
 
-                    <div className={styles.starRating}>
-          {[...Array(5)].map((_, index) => (
-            <StarIcon key={index} className={styles.starIcon} />
-          ))}
-        </div>
-        {!isEditing && (
-                  <div className={styles.buttons}>
-                    <button onClick={handleEditClick}>Editar Comentario</button>       
+                  <div className={styles.starRating}>
+                    <Rating
+                      name={`product-rating-${index}`}
+                      value={rating.puntuation}
+                      className={styles.rating}
+                      // onChange={(event, value) => handleRatingChange(value)}
+                      size="large"
+                    />
                   </div>
-          )}
+                  {!isEditing && (
+                    <div className={styles.buttons}>
+                      <Button variant="contained" onClick={handleEditClick}>
+                        Editar Comentario
+                      </Button>
+                    </div>
+                  )}
                 </div>
-            </div>
-        </Card>
-
-
-    );
+              </div>
+            </Card>
+          );
+        })
+      ) : (
+        <Typography variant="body1" color="text.secondary">
+          No ratings found.
+        </Typography>
+      )}
+    </>
+  );
 };
 
 export default DashboardCardProductUserReview;
