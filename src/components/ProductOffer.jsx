91ac66abb03e8e0c35bc75ef3eee162075fc9bb3
gonzam77 <gonzam77@gmial.com>
2023-07-04 11:47:06 -0300

    feature/dev-GonzaM (frontend/dashboardOffer) add function post delete and edit offers in dashboard

---
+++ src/components/ProductOffer.jsx
@@ -0,0 +1,166 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import EditIcon from '@mui/icons-material/Edit';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import styles from '../assets/styles/components/DashboardCard.module.css';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import swal from 'sweetalert';
+import swal2 from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content'
+import { useNavigate } from 'react-router-dom';
+
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+const DescriptionWrapper = styled('div')(({ theme }) => ({
+    position: 'relative',
+}));
+
+const EditIconWrapper = styled('div')(({ theme }) => ({
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+}));
+
+const DescriptionText = styled(Typography)(({ theme }) => ({
+    wordWrap: 'break-word',
+}));
+
+export default function ProductOffer({
+    id,
+    product_id,
+    product_name,
+    image,
+    description,
+    discount,
+    price,
+}) {
+    const user = useSelector((state) => state.user);
+    const offers = useSelector((state) => state.offer);
+    const [expanded, setExpanded] = React.useState(false);
+    const MySwal = withReactContent(swal2);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleUpdateOffer = async (event) => {
+        event.preventDefault();
+        const offerProduct = offers.find(offer => offer.product_id === product_id);
+        const { value: discount } = await MySwal.fire({
+            title: 'Ingrese el porcentaje de descuento',
+            input: 'text',
+            inputLabel: 'Descuento',
+            inputPlaceholder: 'Ingrese el descuento',
+            showCancelButton: true,
+        })
+        if (discount) {
+            const offer = Number(discount);
+            const response = await axios.put(`/offer/${user.id}`, {
+                discount: offer,
+                offerId: offerProduct.id
+            })
+            if (response.status === 200) {
+                MySwal.fire(`Bien! Ahora el producto ${product_name} tiene un ${discount}% de descuento!`)
+                setTimeout(() => {
+                    window.location.reload();    
+                }, 2000);
+            }
+        }
+    }
+    const handleDeleteOffer = async (event) => {
+        event.preventDefault();
+        swal({
+            title: "Advertencia",
+            text: 'Desea borrar este producto de las ofertas?',
+            button: ['Cancelar', 'Aceptar'],
+        })
+        const response = await axios.delete(`/offer/${id}?userId=${user.id}`)
+        if (response.status === 200) {
+            swal({
+                title: 'Eliminado',
+                text: 'Producto eliminado de las ofertas',
+                icon: 'success'
+            })
+            setTimeout(() => {
+                window.location.reload();             
+            }, 2000);
+        }
+    }
+    
+    return (
+        <Card className={styles.mainContainer}>
+            <div>
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageCard}>
+                        <img src={image} alt="Imagen del vino" />
+                    </div>
+                    <div className={styles.contentIconImage}>
+                        <div></div>
+                        <IconButton >
+                            <DeleteForeverIcon fontSize="small" onClick={handleDeleteOffer} />
+                        </IconButton>
+                        <IconButton>
+                            <EditIcon fontSize="small" onClick={handleUpdateOffer} />
+                        </IconButton>
+                    </div>
+                </div>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {product_name || 'Nombre del producto'}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton>
+                        <AttachMoneyIcon />
+                        {price || 'Price'}
+                    </IconButton>
+                    <IconButton>
+                        <AttachMoneyIcon />
+                        {discount || 'Price'}
+                    </IconButton>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <DescriptionWrapper>
+                            <span>Descripcion:</span>
+                            <DescriptionText paragraph>
+                                {description || 'Descripcion del producto'}
+                            </DescriptionText>
+                        </DescriptionWrapper>
+                    </CardContent>
+                </Collapse>
+            </div>
+        </Card>
+    );
+}
