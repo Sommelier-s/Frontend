68ff63adab1ee7b6cd99f8a494b2fca5a96c85ad
JesseMcCallums <jessemccallums2001@gmail.com>
2023-07-05 20:08:50 -0400

    Feature(final)
    In this commit I am adding the final implementation of posting ratings

--- src/views/DashboardUser.jsx
+++ src/views/DashboardUser.jsx
@@ -1,89 +1,103 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import DashboardMenuUser from "../components/DashboardMenuUser";
-import DashboardCardUser from "../components/DashboardCardUser";
-import DashboardCardProductUser from '../components/DashboardCardProductUser';
-import DashboardCardProductUserReview from '../components/DashboardCardProductUserReview';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import DashboardMenuUser from '../components/DashboardMenuUser'
+import DashboardCardUser from '../components/DashboardCardUser'
+import DashboardCardProductUser from '../components/DashboardCardProductUser'
+import DashboardCardProductUserReview from '../components/DashboardCardProductUserReview'
+import axios from 'axios'
 
-import profile_user from "../assets/img/profile_user.png"
-import imageBack from "../assets/img/imageBack.png";
+import profile_user from '../assets/img/profile_user.png'
+import imageBack from '../assets/img/imageBack.png'
 
-import styles from "../assets/styles/components/views/DashboardUser.module.css";
+import styles from '../assets/styles/components/views/DashboardUser.module.css'
 
 const DashboardUser = () => {
-  const cartProducs = useSelector((state) => state.cart);
-  const [selectedOption, setSelectedOption] = useState('');
-  const [purchasedProducts, setPurchasedProducts] = useState([]);
-  const [productsSaved, setProductsSaved] = useState([]);
-  const [filteredProductsSaved, setFilteredProductsSaved] = useState([]);
-  const navigate = useNavigate();
-  const user = useSelector((state) => state.user);
-  const idUser = user.id;
-
+  const cartProducs = useSelector(state => state.cart)
+  const [selectedOption, setSelectedOption] = useState('')
+  const [purchasedProducts, setPurchasedProducts] = useState([])
+  const [productsSaved, setProductsSaved] = useState([])
+  const [filteredProductsSaved, setFilteredProductsSaved] = useState([])
+  const [userRatings, setUserRatings] = useState([])
+  const navigate = useNavigate()
+  const user = useSelector(state => state.user)
+  const idUser = user.id
+  console.log(productsSaved)
   const getPurchasedProducts = async () => {
     try {
-      const { data } = await axios.get(`/purchased/${idUser}`);
-      setPurchasedProducts(data.data);
-      await fetchProductDetails();
+      const { data } = await axios.get(`/purchased/${idUser}`)
+      setPurchasedProducts(data.data)
+      await fetchProductDetails()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const fetchProductDetails = async () => {
     try {
       const productDetails = await Promise.all(
-        purchasedProducts.map(async (el) => {
-          const { data } = await axios.get(`/both_drinks/?id=${el.product_id}`);
-          return data.data;
+        purchasedProducts.map(async el => {
+          const { data } = await axios.get(`/both_drinks/?id=${el.product_id}`)
+          return data.data
         })
-      );
-      setProductsSaved(productDetails);
+      )
+      setProductsSaved(productDetails)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
+  // const getRatingByUser = async () => {
+  //   try {
+  //     const { data } = await axios.get(`/rating/user/${idUser}`)
+  //     const userRatings = data.data.map(rating => rating.search.id)
+  //     const filteredProducts = productsSaved.filter(product => {
+  //       return !userRatings.includes(product.id)
+  //     })
+  //     setFilteredProductsSaved(filteredProducts)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
   const getRatingByUser = async () => {
-	try {
-	  const { data } = await axios.get(`/rating/user/${idUser}`);
-	  const userRatings = data.data.map((rating) => rating.search.id);
-	  const filteredProducts = productsSaved.filter((product) => {
-		return !userRatings.includes(product.id);
-	  });
-	  setFilteredProductsSaved(filteredProducts);
-	} catch (error) {
-	  console.log(error);
-	}
-  };
-  
-
-  const handleMenu = (option) => {
-    setSelectedOption(option);
-    if (option === 'purchasedProducts') {
-      getAllInformation();
+    try {
+      const { data } = await axios.get(`/rating/user/${idUser}`)
+      const userRatings = data.data.map(rating => rating.search.id)
+      const filteredProducts = productsSaved.filter(product => {
+        return !userRatings.includes(product.id)
+      })
+      setFilteredProductsSaved(filteredProducts)
+      setUserRatings(userRatings)
+    } catch (error) {
+      console.log(error)
     }
-  };
+  }
+
+  const handleMenu = option => {
+    setSelectedOption(option)
+    if (option === 'purchasedProducts') {
+      getAllInformation()
+    }
+  }
 
   const getAllInformation = async () => {
-    await getPurchasedProducts();
-    await fetchProductDetails();
+    await getPurchasedProducts()
+    await fetchProductDetails()
     if (selectedOption === 'purchasedProducts') {
-      await getRatingByUser();
+      await getRatingByUser()
     }
-  };
+  }
 
   useEffect(() => {
-    getAllInformation();
-  }, []);
+    getAllInformation()
+    getRatingByUser()
+  }, [])
 
   return (
     <div className={styles.container}>
       <div className={styles.menuContainer}>
         <button className={styles.botonBack} onClick={() => navigate('/')}>
-          <img className={styles.imageBack} src={imageBack} alt="Volver" />
+          <img className={styles.imageBack} src={imageBack} alt='Volver' />
         </button>
         <DashboardMenuUser onClick={handleMenu} />
       </div>
@@ -92,26 +106,39 @@ const DashboardUser = () => {
           {selectedOption === 'profile' && (
             <DashboardCardUser
               profile_user={profile_user}
-              name="Pedro Romero"
-              email="pedro@gmail.com"
-              password="**********"
+              name='Pedro Romero'
+              email='pedro@gmail.com'
+              password='**********'
             />
           )}
-          {selectedOption === 'purchasedProducts' && (
-            filteredProductsSaved.map((product) => (
-              <DashboardCardProductUser
-                key={product.id}
-                product={product}
-              />
-            ))
-          )}
+          {selectedOption === 'purchasedProducts' &&
+            (filteredProductsSaved.length > 0
+              ? filteredProductsSaved.map(product => {
+                  const isRatedByUser = userRatings.includes(product.id)
+                  return !isRatedByUser ? (
+                    <DashboardCardProductUser
+                      key={product.id}
+                      product={product}
+                    />
+                  ) : null
+                })
+              : productsSaved.map(product => {
+                  const isRatedByUser = userRatings.includes(product.id)
+                  return !isRatedByUser ? (
+                    <DashboardCardProductUser
+                      key={product.id}
+                      product={product}
+                    />
+                  ) : null
+                }))}
+
           {selectedOption === 'updatedReview' && (
             <DashboardCardProductUserReview />
           )}
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default DashboardUser;
+export default DashboardUser
